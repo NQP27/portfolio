a105e6f4d2f1fa633e924cd6aed0a6c3e73e5d35
Nguyễn Quang Phước <phuocquang272@gmail.com>
2025-09-10 12:06:52 +0700

    Fix: Tech Stacks

--- src/app/components/stack/stack.ts
+++ src/app/components/stack/stack.ts
@@ -1,8 +1,11 @@
 import { Component, AfterViewInit, ElementRef, ViewChildren, QueryList, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 interface StackItem {
 	name: string;
 	linkImage: string;
+	alt: string;
+	link: string;
 }
 
 interface StackCategory {
@@ -19,55 +22,60 @@ interface StackCategory {
 export class StackComponent implements AfterViewInit {
 	categories: StackCategory[] = [
 		{
-			name: 'Language',
+			name: 'Languages',
 			items: [
-				{ name: 'C#', linkImage: '/assets/icons/cs.svg' },
-				{ name: 'Python', linkImage: '/assets/icons/python.svg' },
-				{ name: 'TypeScript', linkImage: '/assets/icons/typescript.svg' }
+				{ name: 'C#', linkImage: '/assets/icons/cs.svg', alt: 'C#', link: 'https://dotnet.microsoft.com/en-us/languages/csharp' },
+				{ name: 'Python', linkImage: '/assets/icons/python.svg', alt: 'Python', link: 'https://www.python.org/' },
+				{ name: 'TypeScript', linkImage: '/assets/icons/typescript.svg', alt: 'TypeScript', link: 'https://www.typescriptlang.org/' }
 			]
 		},
 		{
-			name: 'Framework',
+			name: 'Frameworks',
 			items: [
-				{ name: 'ASP.NET Core', linkImage: '/assets/icons/dotnet.svg' },
-				{ name: 'Angular', linkImage: '/assets/icons/angular.svg' },
-				{ name: 'Bootstrap', linkImage: '/assets/icons/bootstrap.svg' },
+				{ name: '.NET', linkImage: '/assets/icons/dotnet.svg', alt: 'ASP.NET Core, Entity Framework Core', link: 'https://dotnet.microsoft.com/en-us/apps/aspnet' },
+				{ name: 'Angular', linkImage: '/assets/icons/angular.svg', alt: 'Angular', link: 'https://angular.io/' },
+				{ name: 'Bootstrap', linkImage: '/assets/icons/bootstrap.svg', alt: 'Bootstrap', link: 'https://getbootstrap.com/' }
 			]
 		},
 		{
-			name: 'ETL Framework',
+			name: 'ETL Frameworks',
 			items: [
-				{ name: 'Airflow', linkImage: '/assets/icons/airflow.svg' },
-				{ name: 'Sparks', linkImage: '/assets/icons/spark.svg' },
+				{ name: 'Airflow', linkImage: '/assets/icons/airflow.svg', alt: 'Apache Airflow', link: 'https://airflow.apache.org/' },
+				{ name: 'Spark', linkImage: '/assets/icons/spark.svg', alt: 'Apache Spark', link: 'https://spark.apache.org/' }
 			]
 		},
 		{
-			name: 'Database',
+			name: 'Databases',
 			items: [
-				{ name: 'SQLServer', linkImage: '/assets/icons/sqlserver.svg' },
-				{ name: 'PostgreSQL', linkImage: '/assets/icons/postgresql.svg' }
+				{ name: 'SQLServer', linkImage: '/assets/icons/sqlserver.svg', alt: 'SQL Server', link: 'https://www.microsoft.com/en-us/sql-server' },
+				{ name: 'PostgreSQL', linkImage: '/assets/icons/postgresql.svg', alt: 'PostgreSQL', link: 'https://www.postgresql.org/' }
 			]
 		},
 		{
-			name: 'Cloud',
+			name: 'Clouds',
 			items: [
-				{ name: 'AWS', linkImage: '/assets/icons/aws.svg' },
-				{ name: 'GCP', linkImage: '/assets/icons/gcp.svg' },
+				{ name: 'AWS', linkImage: '/assets/icons/aws.svg', alt: 'Amazon Web Services', link: 'https://aws.amazon.com/' },
+				{ name: 'GCP', linkImage: '/assets/icons/gcp.svg', alt: 'Google Cloud Platform', link: 'https://cloud.google.com/' }
 			]
 		},
 		{
-			name: 'Other',
+			name: 'Others',
 			items: [
-				{ name: 'Git', linkImage: '/assets/icons/git.svg' },
-				{ name: 'Docker', linkImage: '/assets/icons/docker.svg' },
-				{ name: 'Postman', linkImage: '/assets/icons/postman.svg' },
+				{ name: 'Git', linkImage: '/assets/icons/git.svg', alt: 'Git', link: 'https://git-scm.com/' },
+				{ name: 'Docker', linkImage: '/assets/icons/docker.svg', alt: 'Docker', link: 'https://www.docker.com/' },
+				{ name: 'Postman', linkImage: '/assets/icons/postman.svg', alt: 'Postman', link: 'https://www.postman.com/' }
 			]
-		},
-
+		}
 	];
 
 	@ViewChildren('stackItem') stackItems!: QueryList<ElementRef>;
 	@ViewChild('stackBg') stackBg!: ElementRef<HTMLDivElement>;
+
+	openLink(url: string) {
+		if (url) {
+			window.open(url, '_blank');
+		}
+	}
 
 	ngAfterViewInit() {
 		const observer = new IntersectionObserver(entries => {
